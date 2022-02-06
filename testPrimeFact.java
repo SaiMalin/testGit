f6909e0b621a1f2da700626407f96b91fc32ef68
saivenkateshmalineni <saimalineni33@gmail.com>
2022-02-06 15:24:31 -0500

    Commited 2nd world testPrimeFact

--- testPrimeFact.java
+++ testPrimeFact.java
@@ -3,5 +3,6 @@ public class testPrimeFact {
     public static void main(String args[])
     {
         System.out.println("Hello this is from testPrimeFact");
+        System.out.println("World");
     }
 }
